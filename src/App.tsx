169b9e256314a82c22d908d8444303627bc98122
Sepideh Khadempir <sepideh.khadempir@joingsg.com>
2024-06-22 13:09:51 +0100

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 function App() {
   return (
     <ChakraProvider>
-      <>this is app</>
+      <>this is app test</>
     </ChakraProvider>
   );
 }
