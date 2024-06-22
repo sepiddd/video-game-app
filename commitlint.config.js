289a6d0d79dd44fe4c8044213b3fdba78d6ff577
Sepideh Khadempir <sepideh.khadempir@joingsg.com>
2024-06-22 13:15:12 +0100

    update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,37 +1,26 @@
-module.exports = {
-  parserPreset: "conventional-changelog-conventionalcommits",
+module.exports = { 
+  extends: ['@commitlint/config-conventional'],
+
   rules: {
-    "body-leading-blank": [1, "always"],
-    "body-max-line-length": [2, "always", 100],
-    "footer-leading-blank": [1, "always"],
-    "footer-max-line-length": [2, "always", 100],
-    "header-max-length": [2, "always", 100],
-    "scope-case": [2, "always", "lower-case"],
-    "subject-case": [
-      2,
-      "never",
-      ["sentence-case", "start-case", "pascal-case", "upper-case"],
-    ],
-    "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
-    "type-case": [2, "always", "lower-case"],
-    "type-empty": [2, "never"],
-    "type-enum": [
-      2,
-      "always",
-      [
-        "build",
-        "chore",
-        "ci",
-        "docs",
-        "feat",
-        "fix",
-        "perf",
-        "refactor",
-        "revert",
-        "style",
-        "test",
-      ],
-    ],
-  },
+      'body-case': [2, 'always', 'sentence-case'],
+      'body-max-line-length': [1, 'always', 72],
+      'header-max-length': [1, 'always', 52],
+      'type-enum': [2, 'always', [
+          'build',
+          'change',
+          'chore',
+          'ci',
+          'deprecate',
+          'docs',
+          'feat',
+          'fix',
+          'perf',
+          'refactor',
+          'remove',
+          'revert',
+          'security',
+          'style',
+          'test'
+      ]]
+  }
 };
